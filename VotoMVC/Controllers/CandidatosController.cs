ea50aae35808c0f4478ed8ce3631e58c81d16bce
Sebastián Albacura <sebastianalbacura24@gmail.com>
2026-01-08 07:37:02 -0500

    n21

---
+++ VotoMVC/Controllers/CandidatosController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Voto.ApiConsumer;
+using VotoModelos;
+
+namespace VotoMVC.Controllers
+{
+    public class CandidatosController : Controller
+    {
+        public CandidatosController()
+        {
+            // IMPORTANTE: Si la API en el otro PC usa un puerto específico
+            Voto.ApiConsumer.Crud<VotoModelos.Candidato>.UrlBase = "http://10.241.253.223:8085/api/Candidatos";
+        }
+        // GET: CandidatosController
+        public ActionResult Index()
+        {
+            var apiResult = Crud<Candidato>.ReadAll();
+
+            // Si apiResult es nulo o Data es nulo, mandamos lista vacía
+            var modelo = apiResult?.Data ?? new List<Candidato>();
+
+            return View(modelo);
+        }
+
+        // GET: CandidatosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CandidatosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CandidatosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CandidatosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CandidatosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CandidatosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CandidatosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ VotoMVC/Controllers/VotantesController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VotoMVC.Controllers
+{
+    public class VotantesController : Controller
+    {
+        // GET: VotantesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: VotantesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VotantesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VotantesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VotantesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VotantesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VotantesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VotantesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
