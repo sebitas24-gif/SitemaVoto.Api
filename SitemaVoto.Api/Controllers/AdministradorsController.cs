1a7dea1f732271c6c8e573c351a2255ccd1877e3
Sebastián Albacura <sebastianalbacura24@gmail.com>
2026-01-08 07:05:10 -0500

    n20

--- SitemaVoto.Api/Controllers/AdministradorsController.cs
+++ SitemaVoto.Api/Controllers/AdministradorsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,8 @@ using VotoModelos;
 
 namespace SitemaVoto.Api.Controllers
 {
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Authorize(Roles = "ADMIN")]
     [Route("api/[controller]")]
     [ApiController]
     public class AdministradorsController : ControllerBase
