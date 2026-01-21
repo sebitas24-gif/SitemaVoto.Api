d57b74f25a9def8c8e43fb86ada2f06204a80d73
Sebastián Albacura <sebastianalbacura24@gmail.com>
2026-01-20 22:15:40 -0500

    n41

---
+++ SitemaVoto.Api/Controllers/ResultadosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SitemaVoto.Api.Services.Resultados.Models;
+using SitemaVoto.Api.Services.Resultados;
+
+namespace SitemaVoto.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResultadosController : ControllerBase
+    {
+        private readonly IResultadosService _resultados;
+
+        public ResultadosController(IResultadosService resultados)
+        {
+            _resultados = resultados;
+        }
+
+        // GET: api/Resultados/nacional
+        [HttpGet("nacional")]
+        public async Task<ActionResult<ResultadosResponse>> Nacional(CancellationToken ct)
+        {
+            var r = await _resultados.GetNacionalAsync(ct);
+            return Ok(r);
+        }
+
+        // GET: api/Resultados/provincia/Pichincha
+        [HttpGet("provincia/{provincia}")]
+        public async Task<ActionResult<IReadOnlyList<ResultadoItem>>> PorProvincia(string provincia, CancellationToken ct)
+        {
+            var r = await _resultados.GetPorProvinciaAsync(provincia, ct);
+            return Ok(r);
+        }
+    }
+}
